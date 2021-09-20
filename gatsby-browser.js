ee8d4cef692f7f7e2d13ec599244833c84ea5068
naveen521kk <naveen521kk@gmail.com>
2021-09-20 11:35:05 +0000

    Change fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
 import 'jquery/dist/jquery.min.js'
 import 'popper.js/dist/popper.min'
 import 'bootstrap/dist/js/bootstrap.min.js'
-import "@fontsource/roboto";
+import "@fontsource/ibm-plex-sans";
+import "@fontsource/plus-jakarta-sans";
