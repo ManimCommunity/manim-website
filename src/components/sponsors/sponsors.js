e53bcac7dbf17dbae3873ad3baef28891ee09821
naveen521kk <naveen521kk@gmail.com>
2021-11-06 18:03:30 +0000

    Improve sponsor section

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -9,18 +9,26 @@ const Sponsors = () => {
             <div className="icon-div">
                 <CloudIcon />
             </div>
-
             <h2>Our Sponsors</h2>
             <p>Manim is proudly sponsored by the following organizations</p>
             <div className="sponsor-flex">
                 <div className="sponsor">
-                    <a href="https://tutanota.com/">
+                    <a
+                        href="https://tutanota.com/"
+                        rel="sponsored noreferrer noopener"
+                        target="_blank"
+                    >
                         <StaticImage
-                            src="./tutanota.png"
+                            src="./tutanota-logo-red-black-font.png"
                             alt="Tutanota Logo"
+                            width="300"
                             placeholder="blurred"
                         />
-                        <p>Tutanota</p>
+                        <p className="sponsor-title">Tutanota</p>
+                        <p className="sponsor-textone">
+                            Tutanota is the world's most secure email service,
+                            easy to use and private by design.
+                        </p>
                     </a>
                 </div>
             </div>
