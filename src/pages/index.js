34480f8aa026d2edbc9058f76a5143b6c3ea67a4
naveen521kk <naveen521kk@gmail.com>
2021-11-06 16:36:34 +0000

    Add a tooltip for copy button

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,25 @@ import {Footer} from "../components/footer";
 import Prism from "prismjs";
 import {ManimExample} from "../components/manim-example.js";
 import {Sponsors} from "../components/sponsors/sponsors.js";
-import {GithubIcon, CliIcon, GiftIcon, PypiLogo} from "../components/icons";
+import {
+    GithubIcon,
+    CliIcon,
+    GiftIcon,
+    PypiLogo,
+    CopiedIcon
+} from "../components/icons";
+import Tippy from "@tippyjs/react";
 
+const onClickCopy = () => {
+    if (navigator.clipboard) {
+        const element = document.getElementById("install-manim-command");
+        let text = element.innerText.trim();
+        if (text.startsWith("$")) {
+            text = text.substr(1).trimLeft();
+        }
+        navigator.clipboard.writeText(text);
+    }
+};
 const IndexPage = () => {
     React.useEffect(() => {
         // call the highlightAll() function to style our code blocks
@@ -54,9 +71,29 @@ const IndexPage = () => {
                             <div className="install-manim-pypi-logo">
                                 <PypiLogo />
                             </div>
-                            <div className="install-manim-text">
-                                pip install manim
-                            </div>
+                            <Tippy
+                                content={
+                                    <span className="tippy-text">
+                                        <CopiedIcon />
+                                        Copied to Clipboard
+                                    </span>
+                                }
+                                trigger="click"
+                                interactive={true}
+                                interactiveDebounce="40"
+                                duration={100}
+                            >
+                                <Tippy content="Copy to Clipboard"
+                        >
+                                <div
+                                    className="install-manim-text"
+                                    onClick={onClickCopy}
+                                    id="install-manim-command"
+                                >
+                                    pip install manim
+                                </div>
+                                </Tippy>
+                            </Tippy>
                         </div>
                     </div>
                 </div>
