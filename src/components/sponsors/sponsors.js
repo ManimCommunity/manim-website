2157e5a18424302d8da71b3eae8841015c83533d
naveen521kk <naveen521kk@gmail.com>
2021-11-06 17:33:56 +0000

    Changes to sponsors component

--- src/components/sponsors/sponsors.js
+++ src/components/sponsors/sponsors.js
@@ -1,11 +1,17 @@
 import * as React from "react";
 import "./sponsors.scss";
 import {StaticImage} from "gatsby-plugin-image";
+import {CloudIcon} from "../icons";
 
 const Sponsors = () => {
     return (
-        <div className="sponsors-div">
-            <h2>Sponsors</h2>
+        <div className="sponsors-div" id="sponsors">
+            <div className="icon-div">
+                <CloudIcon />
+            </div>
+
+            <h2>Our Sponsors</h2>
+            <p>Manim is proudly sponsored by the following organizations</p>
             <div className="sponsor-flex">
                 <div className="sponsor">
                     <a href="https://tutanota.com/">
