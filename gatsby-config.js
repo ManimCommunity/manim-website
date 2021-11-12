fec8ea9b4a04b2291747b1483821190d790c625a
naveen521kk <naveen521kk@gmail.com>
2021-11-12 14:44:07 +0000

    Add missing plugin and format

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
         },
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-react-helmet`
     ]
 };
