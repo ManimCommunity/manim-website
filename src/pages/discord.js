0eabee6a1dd5df3b3eeb63190573e003fd8981ff
naveen521kk <naveen521kk@gmail.com>
2021-11-12 14:05:37 +0000

    Format

--- src/pages/discord.js
+++ src/pages/discord.js
@@ -1,7 +1,14 @@
-import React from "react"
-import { Helmet } from "react-helmet"
+import React from "react";
+import {Helmet} from "react-helmet";
 
 const DiscordRedirect = () => {
-    return <Helmet><meta http-equiv="refresh" content="0; url='https://discord.gg/bYCyhM9Kz2'" /></Helmet>;
-}
+    return (
+        <Helmet>
+            <meta
+                http-equiv="refresh"
+                content="0; url='https://discord.gg/bYCyhM9Kz2'"
+            />
+        </Helmet>
+    );
+};
 export default DiscordRedirect;
