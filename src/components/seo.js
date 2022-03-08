92bc14474a6d6ea01009d6ec78e75f6328e2dc5d
ad_chaos <90276965+Kiran-Raj-Dev@users.noreply.github.com>
2022-03-08 18:38:59 +0530

    Fixed blue favicon on Safari (#52)
    Fixes https://github.com/ManimCommunity/manim-website/issues/25
    Also, fixed a typo in `icons.js`.

--- src/components/seo.js
+++ src/components/seo.js
@@ -56,11 +56,6 @@ const SEO = ({title, description, image, article}) => {
                 sizes="16x16"
                 href="/favicon-16x16.png"
             />
-            <link
-                rel="mask-icon"
-                href="/safari-pinned-tab.svg"
-                color="#5bbad5"
-            />
             <meta name="msapplication-TileColor" content="#ffffff" />
             <meta name="theme-color" content="#ece6e2" />
 
