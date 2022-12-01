79f1d11d37c2eac7cee93bda0db5cf9d396edb0f
Naveen M K <naveen521kk@gmail.com>
2022-12-01 19:44:56 +0530

    gatsby-node: update to newer syntax
    fixes a deprecation warning in build

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,7 +28,7 @@ exports.createPages = async ({graphql, actions, reporter}) => {
         `
             {
                 allMarkdownRemark(
-                    sort: {fields: [frontmatter___date], order: ASC}
+                    sort: {frontmatter: {date: ASC}}
                     limit: 500
                 ) {
                     nodes {
